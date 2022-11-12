c1f40109ce1714862a10a2d6e96923072d936ddf
adamfris <adamfris@student.chalmers.se>
2022-11-12 21:58:23 +0100

    Base project

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * testing 1 2 1 2 
+ */
+
+/// Halllå
+int test = 0;
+
+void testFunc();/// test function; 
+/**
+ * @brief bla bla bla bla
+ * 
+ * hihihihihih
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+void testFunc(){
+    test++;
+}
