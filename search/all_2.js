0fa71294e1557e116e46cda3b684bd455fe06630
adams-son <adams-son@users.noreply.github.com>
2022-11-14 15:07:31 +0000

    deploy: 02a79a7dc79981caf7da139b9de98cf72a40d50b

---
+++ search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['test_0',['test',['../lib7SD_8cpp.html#a1bd3f5fbccb1628eb13dda4cd02633a4',1,'lib7SD.cpp']]],
+  ['testfunc_1',['testFunc',['../lib7SD_8cpp.html#a1ca0299241c82d4381fe9a3f2df40d5a',1,'lib7SD.cpp']]]
+];
